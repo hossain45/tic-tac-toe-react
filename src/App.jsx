67f36c51715f74a8d027061b7e23761413645594
hossain45 <hossain45@student.sust.edu>
2023-09-09 17:22:19 +0600

    css done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import TicTacToe from '../components/TicTacToe'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>Tic Tac Toe with REACT</h1>
-      
+    <>      
+      <TicTacToe />
+
     </>
   )
 }
